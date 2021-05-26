a24900c7e0b61e07a25a3d286305145b6d560dda
pentakotasaivenkat <saivenkatpentakota@gmail.com>
2021-05-26 12:08:53 +0530

    BILLING-101 add staging (release) environment

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  primaryFirebaseConfig: {
+    apiKey: "AIzaSyB41rM8gU8Dyk7jxXRbdOStPM8fMtsOxCE",
+    authDomain: "billingsoftware17a-init.firebaseapp.com",
+    projectId: "billingsoftware17a-init",
+    storageBucket: "billingsoftware17a-init.appspot.com",
+    messagingSenderId: "104514130988",
+    appId: "1:104514130988:web:3b5541ad7f5a72795b2d89",
+    measurementId: "G-CEXQN8FCXP"
+  },
+  secondaryFirebaseConifg: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
+};
